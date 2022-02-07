7682a21499359a8ecf7087be853ae4aad8f3c583
usuarioman <castilla.ar@gmail.com>
2022-02-07 13:20:55 -0600

    Setup básico para nuestra app de robots.
    Se ha mandado a tomar por culo todo lo que quita espacio.
    
    Ahora se ha creado una carpeta components donde se van a guardar los componentes que iremos creando a través del curso.
    
    Además se ha creado un componente funcional llamado Card que imprime una imagen y tiene un nombre y un correo.

---
+++ robots/src/components/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+    return(
+        <div>
+            <img src="" alt="Robot photo"/>
+            <div>
+                <h2>Juan Dominguez</h2>
+                <p>juandom@gmail.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
--- robots/src/index.js
+++ robots/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from "./components/Card";
 import reportWebVitals from './reportWebVitals';
 import 'tachyons'
 
 ReactDOM.render(
-    <Hello />,
+    <Card/>,
     document.getElementById('root')
 );
 
