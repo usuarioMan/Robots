3c2bc32802204dac79444fee906f7bb654e94c02
usuarioman <castilla.ar@gmail.com>
2022-02-07 12:03:56 -0600

    Cramos el componente Hello

---
+++ robots/src/Hello.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class Hello extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World!</h1>
+                <p>Welcome to react</p>
+            </div>
+        )
+    };
+}
+
+export default Hello;
