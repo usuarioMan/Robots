b15d95bb433e622c9043ee8fbf10efc5dfcdbd1d
usuarioman <castilla.ar@gmail.com>
2022-02-07 12:01:56 -0600

    Ahora vamos a renderizar nuestro propio componente Hello, por lo que vamos a crearlo.

--- robots/src/index.js
+++ robots/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <App />,
+    <Hello />,
     document.getElementById('root')
 );
 
