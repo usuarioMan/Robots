32b6a9b22259b6f50be9a68a010955ac9aa35aef
usuarioman <castilla.ar@gmail.com>
2022-02-08 13:23:28 -0600

    commit final de la aplicación de los roboamigos.

--- robots/src/App.js
+++ robots/src/App.js
@@ -2,15 +2,22 @@ import React, {Component} from "react";
 import CardList from "./components/CardList";
 import {robots} from "./robots";
 import SearchBox from "./components/SearchBox";
+import Scroll from "./components/Scroll";
 
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots:  [],
             searchField: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({robots: users}))
     }
 
     onSearchChange = (event) => {
@@ -21,16 +28,21 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
         });
-
-        return (
-            <div className={"tc"}>
-                <h1>Robo Amigos</h1>
-                <SearchBox
-                    searchChange={this.onSearchChange}
-                />
-                <CardList robots={filteredRobots}/>
-            </div>
-        );
+        if (this.state.robots.length === 0){
+            return <h1>CARGANDO</h1>
+        } else {
+            return (
+                <div className={"tc"}>
+                    <h1>Robo Amigos</h1>
+                    <SearchBox
+                        searchChange={this.onSearchChange}
+                    />
+                    <Scroll>
+                        <CardList robots={filteredRobots}/>
+                    </Scroll>
+                </div>
+            );
+        }
     }
 }
 
